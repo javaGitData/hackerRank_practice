d888b2f62f36f68303a516ca52aa5baadb097b22
Dipika <dipikatechdips@gmail.com>
2021-05-06 10:16:36 +0530

    added test class and modified Demo class in local repo

---
+++ src/main/java/com/corejava/SalesbyMatch_1/Test.java
@@ -0,0 +1,23 @@
+package com.corejava.SalesbyMatch_1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+
+	
+		   public static void main (String args[]) {
+
+		      // create an array of strings
+		      String a[] = new String[]{"abc","pqr","klm","xyz"};
+
+		      List list1 = Arrays.asList(a);
+
+		      // printing the list
+		      System.out.println("The list is:" + list1);
+		   }
+		
+
+	}
+
+
